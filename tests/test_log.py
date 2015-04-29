e65c9e13acfe7201bc6450a0d508e2c667b370e2
Aki Sasaki <aki@escapewindow.com>
2015-04-28 23:44:14 -0700

    start adding LogMethod testing

--- tests/test_log.py
+++ tests/test_log.py
@@ -2,6 +2,7 @@
 '''
 Test scriptharness.log
 '''
+from copy import deepcopy
 import mock
 import scriptharness.log as log
 import unittest
@@ -9,7 +10,6 @@ import unittest
 
 
 # TestSetLoggingConfig {{{1
-
 class TestSetLoggingConfig(unittest.TestCase):
     '''
     Test scriptharness.log.set_logging_config() method
@@ -31,3 +31,36 @@ class TestSetLoggingConfig(unittest.TestCase):
         '''
         log.set_logging_config(**log.LOGGING_DEFAULTS)
         mock_logging.basicConfig.assert_called_once_with(**log.LOGGING_DEFAULTS)
+
+    @staticmethod
+    @mock.patch('scriptharness.log.logging')
+    def test_kwargs(mock_logging):
+        '''
+        Test set_logging_config with non-default kwargs
+        '''
+        test_kwargs = (
+            {'filemode': 'a'},
+            {'format': '%(message)s'},
+            {'filename': 'x', 'filemode': 'w'},
+        )
+        for orig_kwargs in test_kwargs:
+            log.set_logging_config(**orig_kwargs)
+            kwargs = deepcopy(log.LOGGING_DEFAULTS)
+            kwargs.update(orig_kwargs)
+            mock_logging.basicConfig.assert_called_with(**kwargs)
+
+
+# TestLogMethodInit {{{1
+class TestLogMethodInit(unittest.TestCase):
+    '''
+    Test scriptharness.log.LogMethod.__init__()
+    '''
+    @mock.patch('scriptharness.log.logging')
+    def test_no_kwargs(self, mock_logging):
+        '''
+        Test LogMethod.__init__() with no keyword arguments
+        '''
+        func = 'x'
+        lm = log.LogMethod(func)
+        self.assertEqual(lm.config, log.LogMethod.config)
+        self.assertEqual(lm.func, func)
