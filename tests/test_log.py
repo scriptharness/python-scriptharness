768bb0b2619d3f061336d7d9b423734cb5a5febf
Aki Sasaki <aki@escapewindow.com>
2015-04-28 23:27:39 -0700

    add another test_log test; fix test_log pylint

--- tests/test_log.py
+++ tests/test_log.py
@@ -2,11 +2,10 @@
 '''
 Test scriptharness.log
 '''
-import logging
 import mock
 import scriptharness.log as log
 import unittest
-from scriptharness import ScriptHarnessException
+#from scriptharness import ScriptHarnessException
 
 
 # TestSetLoggingConfig {{{1
@@ -15,9 +14,20 @@ class TestSetLoggingConfig(unittest.TestCase):
     '''
     Test scriptharness.log.set_logging_config() method
     '''
+    @staticmethod
     @mock.patch('scriptharness.log.logging')
-    def test_no_kwargs_set_logging_config(self, mock_logging):
+    def test_no_kwargs(mock_logging):
         '''
+        Test set_logging_config with no arguments
         '''
         log.set_logging_config()
+        mock_logging.basicConfig.assert_called_once_with(**log.LOGGING_DEFAULTS)
+
+    @staticmethod
+    @mock.patch('scriptharness.log.logging')
+    def test_default_kwargs(mock_logging):
+        '''
+        Test set_logging_config with default kwargs
+        '''
+        log.set_logging_config(**log.LOGGING_DEFAULTS)
         mock_logging.basicConfig.assert_called_once_with(**log.LOGGING_DEFAULTS)
