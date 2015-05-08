0c6086f5e8627a945387e57774a3532d9dcdcdbb
Aki Sasaki <aki@escapewindow.com>
2015-05-08 01:25:56 -0700

    cosmetic changes

--- scriptharness/__init__.py
+++ scriptharness/__init__.py
@@ -21,6 +21,9 @@ import six
 @six.python_2_unicode_compatible
 class ScriptHarnessBaseException(Exception):
     """All scriptharness exceptions should inherit this exception.
+
+    However, in most cases you probably want to catch ScriptHarnessException
+    instead.
     """
     def __str__(self):
         """
@@ -36,12 +39,8 @@ class ScriptHarnessException(ScriptHarnessBaseException):
     """All developer-facing exceptions should inherit this class.
 
     There is a problem in how scriptharness is being called.
-    If you want to catch all developer-facing scriptharness exceptions, use
-
-      try::
-          ...
-      except ScriptHarnessException as exc_info::
-          ...
+    If you want to catch all developer-facing scriptharness exceptions,
+    catch ScriptHarnessException.
     """
 
 class ScriptHarnessFailure(ScriptHarnessBaseException):
@@ -51,6 +50,26 @@ class ScriptHarnessFailure(ScriptHarnessBaseException):
     This exception should result in program termination; using try/except may
     result in unexpected or dangerous behavior.
     """
+
+# py2 unicode help.  This may move into a separate file later.
+def to_unicode(obj, encoding='utf-8'):
+    """Encode a string as unicode in python2.
+
+    http://farmdev.com/talks/unicode/
+
+    Args:
+        obj (str): the string to encode
+        encoding (str, optional): the encoding to use
+
+    Returns:
+        obj (unicode): the encoded string
+    """
+    if not isinstance(obj, six.text_type):
+        for string_type in six.string_types:
+            if isinstance(obj, string_type):  # pragma: no branch
+                obj = six.text_type(obj, encoding)
+                continue
+    return obj
 
 
 # get_version_string {{{1
@@ -84,23 +103,3 @@ def get_version_string(version):
 # Semantic versioning 2.0.0  http://semver.org/
 __version__ = (0, 1, 0, 'alpha')
 __version_string__ = get_version_string(__version__)
-
-# py2 unicode help.  This may move into a separate file later.
-def to_unicode(obj, encoding='utf-8'):
-    """Encode a string as unicode in python2.
-
-    http://farmdev.com/talks/unicode/
-
-    Args:
-        obj (str): the string to encode
-        encoding (str, optional): the encoding to use
-
-    Returns:
-        obj (unicode): the encoded string
-    """
-    if not isinstance(obj, six.text_type):
-        for string_type in six.string_types:
-            if isinstance(obj, string_type):  # pragma: no branch
-                obj = six.text_type(obj, encoding)
-                continue
-    return obj
