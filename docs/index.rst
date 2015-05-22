e2b12f5e00360b827d7df08971ade9e0f49bf613
Aki Sasaki <aki@escapewindow.com>
2015-05-22 01:09:40 -0700

    quickstart docs!

--- docs/index.rst
+++ docs/index.rst
@@ -3,24 +3,22 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-scriptharness
+Scriptharness
 =============
 
 Scriptharness is a framework for writing scripts.  There are three core principles: full logging, flexible configuration, and modular actions.  The goal of `full logging` is to be able to debug problems purely through the log.  The goal of `flexible configuration` is to make each script useful in a variety of contexts and environments.  The goals of `modular actions` are a) faster development feedback loops and b) different workflows for different usage requirements.
 
 .. _mozharness: https://wiki.mozilla.org/ReleaseEngineering/Mozharness
 
-Quick Start
-===========
-
-TODO: writeme!
+.. include:: quickstart.rst
 
 
 Contents:
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 4
 
+   quickstart
    scriptharness
 
 Indices and tables
--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# This file is formatted slightly differently for readability in ReadTheDocs.
 """python-scriptharness quickstart example.
 """
 from __future__ import absolute_import, division, print_function, \
                        unicode_literals
 import scriptharness
 
-# First, define functions for all actions.  Each action MUST have a function
-# defined.
-#
-# Each action function should be idempotent, and able to run standalone.
-# In this example, 'package' may require that the steps in 'build' ran at
-# some point before 'package' is run, but we can't assume that happened in
-# the same script run.  It could have happened yesterday, or three weeks ago,
-# and 'package' should still be able to run.  If you need to save state
-# between actions, consider saving state to disk.
+
+"""First, define functions for all actions.  Each action MUST have a function
+defined.
+
+Each action function should be idempotent, and able to run standalone.
+In this example, 'package' may require that the steps in 'build' ran at
+some point before 'package' is run, but we can't assume that happened in
+the same script run.  It could have happened yesterday, or three weeks ago,
+and 'package' should still be able to run.  If you need to save state
+between actions, consider saving state to disk.
+"""
 def clobber(config):
     """Clobber the source"""
     ...
@@ -39,47 +42,54 @@ def notify(config):
     """Notify watchers"""
     ...
 
+
 if __name__ == '__main__':
-    # Enable logging to screen + log.txt.  Not required, but without it
-    # the script will run silently.
+    """Enable logging to screen + log.txt.  Not required, but without it
+    the script will run silently.
+    """
     scriptharness.prepare_simple_logging("log.txt")
 
-    # Define actions.  All six actions are available to run, but if the
-    # script is run without any action commandline options, only the
-    # enabled actions will run.
-    #
-    # If default_actions is specified, it MUST be a subset of all_actions
-    # (the first list), and any actions in default_actions will be enabled
-    # by default (the others will be disabled).  If default_actions isn't
-    # specified, all the actions are enabled.
-    #
-    # Each action MUST have a function defined (see above).
+    """Define actions.  All six actions are available to run, but if the
+    script is run without any action commandline options, only the
+    enabled actions will run.
+
+    If default_actions is specified, it MUST be a subset of all_actions
+    (the first list), and any actions in default_actions will be enabled
+    by default (the others will be disabled).  If default_actions isn't
+    specified, all the actions are enabled.
+
+    Each action MUST have a function defined (see above).
+    """
     actions = scriptharness.get_actions_from_list(
         ["clobber", "pull", "build", "package", "upload", "notify"],
         default_actions=["pull", "build", "package"]
     )
 
-    # Create a commandline argument parser, with default scriptharness
-    # argument options pre-populated.
+    """Create a commandline argument parser, with default scriptharness
+    argument options pre-populated.
+    """
     parser = scriptharness.get_parser(all_actions=actions)
 
-    # Add new commandline argument(s)
+    """Add new commandline argument(s)
+    """
     parser.add_argument("--new-argument", action='store',
                         help="help message for --new-argument")
 
-    # Create the Script object.  If this is run a second time, it will
-    # retrieve the same-named script object.  ('name' in get_script() defaults
-    # to "root".  We'll explore running multiple Script objects within the
-    # same script in the not-distant future.)
-    #
-    # When this Script object is created, it will parse all commandline
-    # arguments sent to the script.  So it doesn't matter that this script
-    # (quickstart.py) didn't have the --new-argument option until one line
-    # above; the Script object will parse it and store the new_argument
-    # value in its config.
+    """Create the Script object.  If this is run a second time, it will
+    retrieve the same-named script object.  ('name' in get_script() defaults
+    to "root".  We'll explore running multiple Script objects within the
+    same script in the not-distant future.)
+
+    When this Script object is created, it will parse all commandline
+    arguments sent to the script.  So it doesn't matter that this script
+    (quickstart.py) didn't have the --new-argument option until one line
+    above; the Script object will parse it and store the new_argument
+    value in its config.
+    """
     script = scriptharness.get_script(actions, parser)
 
-    # This will run the script.
-    # Essentially, it will go through the list of actions, and if the action
-    # is enabled, it will run the associated function.
+    """This will run the script.
+    Essentially, it will go through the list of actions, and if the action
+    is enabled, it will run the associated function.
+    """
     script.run()
