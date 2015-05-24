ffeb197d388a46f2059ef6338554cc279838e6f2
Aki Sasaki <aki@escapewindow.com>
2015-05-23 18:19:34 -0700

    make run_tests a little less verbose

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 coverage erase
-tox && coverage html
-rm -f pylint.out
-files=$(find tests scriptharness -type f -name \*.py)
-for file in $files ; do
-    echo $file
-    pylint $file 2>&1 | tee -a pylint.out | grep 'Your code has been rated at '
-done
+tox && coverage html && {
+    files=$(find tests scriptharness -type f -name \*.py)
+    echo "Running pylint..."
+    for file in $files ; do
+        echo -n "."
+        output=$(pylint $file 2>&1 | grep 'Your code has been rated at ')
+        echo $output | grep -q 'rated at 10.00/10'
+        if [ $? != 0 ] ; then
+            echo
+            echo $file: $output
+        fi
+    done
+    echo
+}
